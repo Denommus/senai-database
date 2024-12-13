d7c5ec5aef59e8a45c609f5ab432d9bc45cf145f
Yuri Albuquerque <yuridenommus@gmail.com>
2024-12-13 18:39:04 -0300

    Add mysqlclient to the script

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from here!")
+import pandas as pd
+import MySQLdb as sql
 
+def main():
+    dataset = pd.read_csv("netflix_titles.csv", sep=",")
+    for (index, row) in dataset.iterrows():
+        print(row)
 
 if __name__ == "__main__":
     main()
